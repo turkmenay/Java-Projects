76a8d693fd674974a397d9d5fc839614a03d6160
Ahmet Turkmenay <96058338+turkmenay@users.noreply.github.com>
2024-01-06 22:38:25 +0300

    Add files via upload

---
+++ PrintBCharacter/src/PrintBCharacter.java
@@ -0,0 +1,30 @@
+public class PrintBCharacter {
+    public static void main(String[] args) {
+    String[][] star = new String[7][4];
+    for (int i=0; i<star.length; i++){
+        for (int j=0; j<star[i].length; j++){
+            if(i==0 || i==3 || i==6){
+                if(j==0 || j==1 || j==2){
+                    star[i][j]=" * ";
+                }
+                else
+                    star[i][j]="   ";
+            }
+            else{
+                if(j==0 || j==3){
+                    star[i][j]=" * ";
+                }
+                else
+                    star[i][j]="   ";
+            }
+            }
+        }
+        for (String[] row:star) {
+            for (String col:row){
+                System.out.print(col);
+            }
+            System.out.println();
+        }
+    }
+
+}
